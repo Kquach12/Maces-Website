df24e1edd012d1a6cd57b0303d655fbc4b2694cb
Kenny Quach <kennyquach60@gmail.com>
2022-02-20 11:04:09 -0800

    Display pleyer's stats from db

---
+++ calendar/flask_app/controllers/stats.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, request, session, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_app.models.stat import Stat
+from flask_app.models.season import Season
+
+@app.route('/stats')
+def stats():
+    if 'user_id' not in session:
+        redirect('/')
+
+    season = Season.get_most_recent()
+
+    if season:
+        data ={
+            'season_id': season.id
+        }
+        players_stats = Stat.get_average_of_all_players_by_season(data)
+
+    return render_template('statistics.html', players_stats = players_stats)
--- calendar/server.py
+++ calendar/server.py
@@ -1,4 +1,4 @@
-from flask_app.controllers import users, players, games, notes
+from flask_app.controllers import users, players, games, notes, stats
 from flask_app import app
 
 if __name__ == '__main__':
