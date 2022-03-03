3ab16ea5e8447107676c1b79e8ff48fa5395e870
Joyce-w <hellojoycewong@gmail.com>
2022-03-02 17:52:26 -0800

    dynamically add seasons & players on page

--- calendar/flask_app/controllers/players.py
+++ calendar/flask_app/controllers/players.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, redirect, request, session, flash, url_for
 from flask_app import app
 from flask_app.models.player import Player
+from flask_app.models.season import Season
 from flask_app.models.user import User
-
 
 
 @app.route('/roster')
@@ -13,7 +13,17 @@ def roster():
             "id": session['user_id']
         }
         user = User.get_one(data)
-    varsity, jv = Player.get_season_info()
-    print(varsity)
-    return render_template('roster.html', varsity = varsity, jv=jv, user = user)
+
+    # Get seasons
+    seasons = Season.get_all()
+
+    # load most recent players when page renders
+    recentVar, recentJv = Player.get_season_info({'season': 5});
+
+    #get players based on dropdown selection
+    selected_season = request.args.get('season')
+    data ={'season': selected_season};
+    varsity, jv = Player.get_season_info(data);
+
+    return render_template('roster.html',seasons = seasons, varsity = varsity, jv=jv, user = user, recentVar=recentVar, recentJv=recentJv)
 
